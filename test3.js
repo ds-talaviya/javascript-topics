addcefacd1f512b51b7b2c8ef33e89106c2dfe4c
Dhruvil <Dhruvil@buildesk.in>
2024-04-11 11:05:09 +0530

    flat() method: [1, 2, 3, , [4, 5, [6], [7, [8, [9, [10]]]]]] > [   1, 2, 3, 4,  5,   6, 7, 8, 9, 10 ]

---
+++ test3.js
@@ -0,0 +1,14 @@
+let flat = [1, 2, 3, , [4, 5]];
+console.log("flat.flat()", flat.flat())
+
+let flat2 = [1, 2, 3, , [4, 5, [6]]];
+console.log("flat2.flat()", flat2.flat())
+console.log("flat2.flat(1)", flat2.flat(1))
+console.log("flat2.flat(2)", flat2.flat(2))
+
+let flat3 = [1, 2, 3, , [4, 5, [6], [7, [8, [9, [10]]]]]];
+console.log("flat3.flat()", flat3.flat())
+console.log("flat3.flat(1)", flat3.flat(1))
+console.log("flat3.flat(2)", flat3.flat(2))
+console.log("flat3.flat(3)", flat3.flat(3))
+console.log("flat3.flat(Infinity)", flat3.flat(Infinity))
