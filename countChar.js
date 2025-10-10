049eebc1b9b8741634dd8a1b6dc704e237a67d8f
Dhruvil <Dhruvil@buildesk.in>
2025-10-11 02:49:49 +0530

    changes, add effective solutions

--- countChar.js
+++ countChar.js
@@ -1,14 +1,19 @@
 function countChar(char) {
-    let result = {};
-    for (let i = 0; i < char.length; i++) {
-        let element = char[i];
-        if (result[element]) {
-            result[element] += 1;
-        } else {
-            result[element] = 1;
-        }
-    };
-    return result;
+    // let result = {};
+    // for (let i = 0; i < char.length; i++) {
+    //     let element = char[i];
+    //     if (result[element]) {
+    //         result[element] += 1;
+    //     } else {
+    //         result[element] = 1;
+    //     }
+    // };
+    // return result;
+
+    return char.split('').reduce((acc, value) => {
+        acc[value] = (acc[value] || 0) + 1;
+        return acc
+    }, {})
 }
 
 console.log(countChar("apple"))
--- sumDigitUntilOneDigit.js
+++ sumDigitUntilOneDigit.js
@@ -1,14 +1,21 @@
 function sumDigitUntilOneDigit(num) {
-    let sum = 0;
-    do {
-        sum += num % 10;
-        num = Math.floor(num / 10);
+    // let sum = 0;
+    // do {
+    //     sum += num % 10;
+    //     num = Math.floor(num / 10);
 
-        if (num <= 0 && sum > 9) {
-            num = sum;
-            sum = 0;
-        }
-    } while (num > 0)
-    return sum;
+    //     if (num <= 0 && sum > 9) {
+    //         num = sum;
+    //         sum = 0;
+    //     }
+    // } while (num > 0)
+    // return sum;
+
+    const sum = (str) => {
+        let total = str.toString().split('').reduce((a, b) => a + +b, 0);
+        if (total <= 9) return total
+        return sum(total)
+    }
+    return sum(num)
 }
 console.log(sumDigitUntilOneDigit(532))
